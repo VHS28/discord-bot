300291006673f2526388b09fd15b51d954595c7d
obstel <christian.oberstaedt@googlemail.com>
2026-05-23 14:58:41 +0200

    Enable guild member partial events

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -1,7 +1,8 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Partials } from "discord.js";
 
 export function createDiscordClient(): Client {
   return new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+    partials: [Partials.GuildMember],
   });
 }
